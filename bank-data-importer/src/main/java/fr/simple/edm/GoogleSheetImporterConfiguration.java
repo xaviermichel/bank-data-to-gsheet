b49fcb88a42a6235633256ec6f6ee7ea080cd5a9
xaviermichel <xavier.michel.mx440@gmail.com>
2020-08-05 22:09:57 +0200

    oauth: Add device name and id

--- bank-data-importer/src/main/java/fr/simple/edm/GoogleSheetImporterConfiguration.java
+++ bank-data-importer/src/main/java/fr/simple/edm/GoogleSheetImporterConfiguration.java
@@ -14,6 +14,8 @@ public class GoogleSheetImporterConfiguration {
     private String tokenDirectoryPath;
     private String spreadsheetId;
     private String oauthCallbackIP = "127.0.0.1";
+    private Integer oauthCallbackPort = 8888;
+    private String oauthDevice;
     private Sheet smartInsertSheet;
     private Sheet nextMonthSheet;
     private Sheet smartInsertSheetFormulaSource;
--- bank-data-importer/src/main/java/fr/simple/edm/service/GoogleSheetUtils.java
+++ bank-data-importer/src/main/java/fr/simple/edm/service/GoogleSheetUtils.java
@@ -6,6 +6,7 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -60,8 +61,11 @@ public class GoogleSheetUtils {
             .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(googleSheetImporterConfiguration.getTokenDirectoryPath())))
             .setAccessType("offline")
             .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost(googleSheetImporterConfiguration.getOauthCallbackIP()).setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder()
+            .setHost(googleSheetImporterConfiguration.getOauthCallbackIP())
+            .setPort(googleSheetImporterConfiguration.getOauthCallbackPort())
+            .build();
+        return new EdmAuthorizationCodeInstalledApp(flow, receiver, googleSheetImporterConfiguration).authorize("user");
     }
 
     public void clearRange(String spreadsheetId, String range) throws IOException {
